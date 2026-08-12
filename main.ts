84efc33afcd59d5e67988a4fe42c71c01615ec21
KnorpelSenf <shtrog@gmail.com>
2026-08-12 21:49:16 +0200

    feat: add basic mini app

--- main.ts
+++ main.ts
@@ -1,7 +1,33 @@
-import { Bot, webhookAdapters } from "grammy";
+import { Hono } from "@hono/hono";
+import { html } from "@hono/hono/html";
+import { Bot, InlineKeyboard, webhookAdapters } from "grammy";
 
 const bot = new Bot(Deno.env.get("BOT_TOKEN") ?? "");
+const url = Deno.env.get("BOT_ENDPOINT") ??
+  (await bot.api.getWebhookInfo()).url;
 
-bot.on("message", (ctx) => ctx.send("reply"));
+bot.on("message", async (ctx) => {
+  await ctx.sendMessage("reply", {
+    reply_markup: new InlineKeyboard().webApp("edit", url),
+  });
+});
 
-Deno.serve(webhookAdapters.stdHttp(bot));
+const app = new Hono();
+
+app.get("/", (ctx) =>
+  ctx.html(html`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+            ${bot.me.first_name}
+          </body>
+    </html>
+  `));
+
+app.post("/", webhookAdapters.hono(bot));
+
+Deno.serve(app.fetch);
